1e8f03b3f1c50dbb77dc1db40bb5f7ff92838d28
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-07 04:48:28 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,10 @@ def loop_iterator(number_of_times)
   counter = 0
   while counter < number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  puts phrase
+  break if counter > number_of_times
+  counter += 1
+  end
 end
 
 def times_iterator(number_of_times)
